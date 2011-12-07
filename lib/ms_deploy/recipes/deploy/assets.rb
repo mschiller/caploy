1173e260d9701f6a1e8243687bb0e03ff4926d37
Michael <michael.schiller@gmx.de>
2011-12-07 12:51:50 +0100

    fixed asset precompile support, bumped version

--- lib/ms_deploy/recipes/deploy/assets.rb
+++ lib/ms_deploy/recipes/deploy/assets.rb
@@ -1,17 +1,13 @@
 Capistrano::Configuration.instance.load do
 
   namespace :deploy do
-    namespace :assets, :roles => :web do
+    namespace :assets do
       desc "Compile assets"
-      task :compile do
+      task :compile, :roles => :web do
         run "cd #{release_path}; RAILS_ENV=#{rails_env} #{rake} assets:precompile"
       end
 
-      task :symlink, :roles => :web, :except => {:no_release => true} do
-        # fixme do nothing, rails 3.1
-      end
-
-      after 'deploy:symlink_dependencies', 'deploy:assets:compile'
+      after 'deploy', 'deploy:assets:compile'
 
     end
   end
