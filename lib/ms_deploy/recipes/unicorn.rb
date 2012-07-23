8b776b042d2e7ca0b084d15bb65d0099c538d4d9
Michael Schiller <michael.schiller@highed-solutions.com>
2012-07-23 14:33:26 +0200

    fixed unicorn, bumped version

--- lib/ms_deploy/recipes/unicorn.rb
+++ lib/ms_deploy/recipes/unicorn.rb
@@ -1,9 +1,7 @@
 Capistrano::Configuration.instance.load do
 
   set :unicorn_bin, "bundle exec unicorn"
-  set :unicorn_config, "config/unicorn.#{fetch(:stage, 'production')}.rb"
   set :unicorn_pid, "tmp/pids/unicorn.pid"
-
   #require "capistrano-unicorn"
 
   namespace :deploy do
@@ -15,6 +13,7 @@ Capistrano::Configuration.instance.load do
 
     desc "Start unicorn"
     task :start, :except => { :no_release => true } do
+      set :unicorn_config, "config/unicorn.#{fetch(:stage, 'production')}.rb"
       run "cd #{current_path} && #{try_sudo} #{unicorn_bin} -c #{current_path}/#{unicorn_config} -E #{rails_env} -D"
     end
 
