2d579563100497bb9dc9ce0e6b85dca3d85912bf
Michael Schiller <michael.schiller@highed-solutions.com>
2012-09-12 14:51:24 +0200

    small seeding fix bumped version

--- lib/ms_deploy/recipes/seeding.rb
+++ lib/ms_deploy/recipes/seeding.rb
@@ -1,7 +1,7 @@
 Capistrano::Configuration.instance.load do
 
   namespace :deploy do
-    task :seed do
+    task :seed, :roles => :app do
       run "cd #{current_path} && RAILS_ENV=#{rails_env} bundle exec rake db:seed"
     end
   end
