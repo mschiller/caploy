993d9c33f4b25fe6c13e1d792db72ae1e2eea300
Johannes Vetter <jvse@gmx.at>
2012-11-06 09:35:39 +0100

    - Fix typo to make the various steps work. - Do not bind a :flush to the deploy tasks

--- lib/caploy/recipes/memcache.rb
+++ lib/caploy/recipes/memcache.rb
@@ -1,7 +1,7 @@
 Capistrano::Configuration.instance.load do
 
   namespace :memcache do
-    %(start stop restart status).each do |step|
+    %w(start stop restart status).each do |step|
       desc "#{step} memcache"
       task step do
         try_sudo "service memcached #{step}"
@@ -12,9 +12,6 @@ Capistrano::Configuration.instance.load do
     task :flush do
       run %Q{echo "flush_all" | nc -q 2 127.0.0.1 11211}
     end
-
-    after "deploy:restart", "memcache:flush"
-    after "deploy:start", "memcache:flush"
   end
 
 end
