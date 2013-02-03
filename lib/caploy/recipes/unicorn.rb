7d3b13adfd06b6c68c484abeb8f64104f8fc690b
Michael Schiller <michael.schiller@highed-solutions.com>
2013-02-03 12:30:32 +0100

    fixed unicorn setup task

--- lib/caploy/recipes/unicorn.rb
+++ lib/caploy/recipes/unicorn.rb
@@ -103,29 +103,28 @@ module Unicorn
               end
             end
           end
+
+          desc "Setup unicorn"
+          task :setup, :roles => :app, :except => {:no_release => true} do
+            run "mkdir -p \"#{shared_path}/config/unicorn\""
+            config_path = "#{shared_path}/config/unicorn/#{rails_env}.rb"
+            template_path = File.expand_path('../../templates/unicorn/unicorn.rb.erb', __FILE__)
+            vars = {
+                'application' => application,
+                'current_path' => current_path,
+                'unicorn_pid' => unicorn_pid,
+                'unicorn_std_log' => unicorn_std_log,
+                'unicorn_err_log' => unicorn_err_log,
+                'stage' => stage,
+                'unicorn_listen_backlog' => unicorn_listen_backlog,
+                'unicorn_worker_processes' => unicorn_worker_processes,
+                'sidekiq_redis_count' => sidekiq_redis_count
+            }
+            put(render_erb_template(template_path, vars), config_path)
+          end
         end
 
-        after "deploy:restart", "unicorn:reload"
-
-        desc "Setup unicorn"
-        task :setup, :roles => :app, :except => {:no_release => true} do
-          run "mkdir -p \"#{shared_path}/config/unicorn\""
-          config_path = "#{shared_path}/config/unicorn/#{rails_env}.rb"
-          template_path = File.expand_path('../../templates/unicorn/unicorn.rb.erb', __FILE__)
-          vars = {
-              'application' => application,
-              'current_path' => current_path,
-              'unicorn_pid' => unicorn_pid,
-              'unicorn_std_log' => unicorn_std_log,
-              'unicorn_err_log' => unicorn_err_log,
-              'stage' => stage,
-              'unicorn_listen_backlog' => unicorn_listen_backlog,
-              'unicorn_worker_processes' => unicorn_worker_processes,
-              'sidekiq_redis_count' => sidekiq_redis_count
-          }
-          put(render_erb_template(template_path, vars), config_path)
-        end
-
+        after :"deploy:restart", :"unicorn:reload"
         after :"deploy:setup", :"unicorn:setup";
 
         namespace :deploy do
